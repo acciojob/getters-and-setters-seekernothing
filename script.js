1f98f97aa88999703c143930878750aa3aa31653
ABHISHEK BIRADAR <162307955+seekernothing@users.noreply.github.com>
2025-01-29 07:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this._name= name;
-		this._age=age;
-	}
-	get name(){
-		return this._name;
-	}
-	
-	get age() {
+  constructor(name, age) {
+    this._name = name; // Use _name instead of name
+    this._age = age;   // Use _age instead of age
+  }
+
+  get name() {
+    return this._name; // Getter for name
+  }
+
+  get age() {
     return this._age; // Getter for age
   }
-	
-	set age(){
-		this._age=value;
-	}
+
+  set age(value) {
+    this._age = value; // Setter for age
+  }
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`)
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
